bc668fa3de6378a8af13f8516c1ff1962e1fa54d
Andreza Telles Cascais <shareingprojects@gmail.com>
2020-06-04 17:43:24 -0300

    second-commit

---
+++ src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,26 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    //create a table
+    return knex.schema.createTable('point_items', table=> {
+        table.increments('id').primary();
+
+        table.integer('point_id')
+             .notNullable()
+             .references('id')
+             .inTable('points');
+
+
+        table.string('item_id')
+             .notNullable()
+             .references('id')
+             .inTable('items');
+        
+    });
+}
+
+export async function down(knex: Knex) {
+   //come back (delete table)
+   return knex.schema.dropTable('point_items');
+
+}
